cf9b62280310c5ed5f801a045ab7be0926dcb17c
qbix213 <gryigry3@gmail.com>
2020-07-01 13:49:26 +0200

    h

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const Discord = require("discord.js");
 const config = require("./config.json");
 const ytdl = require('ytdl-core');
-const { prefix, token } = require("./config.json");
 
 
 const client = new Discord.Client();
