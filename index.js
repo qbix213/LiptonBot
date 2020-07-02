7d348c40f67838da778c517682394bf5389f90ee
qbix213 <gryigry3@gmail.com>
2020-07-02 19:13:52 +0200

    Sukces i błąd:

--- index.js
+++ index.js
@@ -504,23 +504,23 @@ client.on('message', async message => {
             .kick('Optional reason that will display in the audit logs')
             .then(() => {
               // We let the message author know we were able to kick the person
-              message.reply(`Pomyślnie skickowano ${user.tag}`);
+              message.channel.send(`:white_check_mark:**Sukces**`);
             })
             .catch(err => {
               // An error happened
               // This is generally due to the bot not being able to kick the member,
               // either due to missing permissions or role hierarchy
-              message.reply('Nie mogę wyrzucić tego użytkownika z serwera :cry:');
+              message.channel.send(':exclamation:**Błąd**');
               // Log the error
               console.error(err);
             });
         } else {
           // The mentioned user isn't in this guild
-          message.reply("That user isn't in this guild!");
+          message.channel.send(":exclamation:**Błąd**");
         }
         // Otherwise, if no user was mentioned
       } else {
-        message.reply("Nie masz kogo skickować!");
+        message.channel.send(":exclamation:**Błąd**");
       }
     }
   
@@ -554,23 +554,23 @@ client.on('message', async message => {
             })
             .then(() => {
               // We let the message author know we were able to ban the person
-              message.reply(`Pomyślnie zbanowano ${user.tag}`);
+              message.channel.send(`:white_check_mark:**Sukces**`);
             })
             .catch(err => {
               // An error happened
               // This is generally due to the bot not being able to ban the member,
               // either due to missing permissions or role hierarchy
-              message.reply(':exclamation:**Błąd**');
+              message.channel.send(':exclamation:**Błąd**');
               // Log the error
               console.error(err);
             });
         } else {
           // The mentioned user isn't in this guild
-          message.reply("Tego użytkownika nie ma na serwerze!");
+          message.channel.send(":exclamation:**Błąd**");
         }
       } else {
         // Otherwise, if no user was mentioned
-        message.reply("Nie masz kogo zbanować!");
+        message.reply(":exclamation:**Błąd**");
       }
     }
 
