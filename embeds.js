8e6498f40bd3491006b17e2417f50d2b3ca4fe3e
qbix213 <gryigry3@gmail.com>
2020-08-17 09:38:14 +0200

    Embedy

--- embeds.js
+++ embeds.js
@@ -1,15 +1,38 @@
 const { MessageEmbed } = require('discord.js')
 exports.embeds = async message => {
     // If the message is "how to embed"
-    if (message.content === '$embed') {
+    if (message.content === '$ja') {
       // We can create embeds using the MessageEmbed constructor
       // Read more about all that you can do with the constructor
       // over at https://discord.js.org/#/docs/main/master/class/MessageEmbed
       const embed = new MessageEmbed()
         .setTitle('A slick little embed')
-        .setColor(0xff0000)
+        .setColor(0x1714B6)
         .setDescription('Hello, this is a slick embed!');
       message.channel.send(embed);
     }
+
+    if (message.content === '$profil') {
+      // We can create embeds using the MessageEmbed constructor
+      // Read more about all that you can do with the constructor
+      // over at https://discord.js.org/#/docs/main/master/class/MessageEmbed
+      const embed = new MessageEmbed()
+        .setTitle('A slick little embed')
+        .setColor(0x1714B6)
+        .setDescription('Hello, this is a slick embed!');
+      message.channel.send(embed);
+    }
+
+    if (message.content === '$profile') {
+      // We can create embeds using the MessageEmbed constructor
+      // Read more about all that you can do with the constructor
+      // over at https://discord.js.org/#/docs/main/master/class/MessageEmbed
+      const embed = new MessageEmbed()
+        .setTitle('A slick little embed')
+        .setColor(0x1714B6)
+        .setDescription('Hello, this is a slick embed!');
+      message.channel.send(embed);
+    }
+
 
   };
